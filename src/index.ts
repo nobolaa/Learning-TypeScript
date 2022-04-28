5123d58b82a65830fc4064fa53d2545269938416
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-28 20:13:57 +0200

    Playing with different type of variables

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-const userName = 'Bezael';
-const age = 33;
+let myTypeString: string = 'Hello world!';
+let myTypeNumber: number = 33;
+let myTypeBoolean: boolean = true;
 
-console.log(`Hello world!, my name is ${userName} and my age is ${age}`);
+let arrNumber : number[] = [1, 2, 3];
+let arrStrings : string[] = ['Uno', "Dos"];
+
+let arrAny : any[] = ['Hola', 33, false];
+
+let tuplePlayer: [string, number, boolean] = ['Doncic', 2, true];
+
+let players: [number, string][];
+
+players = [
+    [1, 'Lebron'],
+    [2, 'Durant'],
+    [3, 'Carry']
+];
+
+let myUnion: string | number | null;
+myUnion = 'hola';
+myUnion = '33';
+myUnion = null;
